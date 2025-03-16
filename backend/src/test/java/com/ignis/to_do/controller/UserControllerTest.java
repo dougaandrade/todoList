6127132defadf5bc096eff950854f0d1e9c890ce
ViniDias1 <viniciusdvalenca@gmail.com>
2025-03-16 01:24:56 -0300

    impl UserControllerTest

---
+++ backend/src/test/java/com/ignis/to_do/controller/UserControllerTest.java
@@ -0,0 +1,65 @@
+package com.ignis.to_do.controller;
+
+import com.ignis.to_do.dto.UserDTO;
+import com.ignis.to_do.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+class UserControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private UserController userController;
+
+    @BeforeEach
+    void setUp() {
+
+        MockitoAnnotations.openMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+    }
+
+    @Test
+    void testCreateUser() throws Exception {
+        UserDTO userDTO = new UserDTO(1L, "João", "joao@email.com", "senha123");
+
+        when(userService.createUser(any(UserDTO.class))).thenReturn(userDTO);
+
+        mockMvc.perform(post("/users")
+                        .contentType("application/json")
+                        .content("{\"name\":\"João\",\"email\":\"joao@email.com\",\"password\":\"senha123\"}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("João"))
+                .andExpect(jsonPath("$.email").value("joao@email.com"))
+                .andExpect(jsonPath("$.password").value("senha123"));
+
+        verify(userService, times(1)).createUser(any(UserDTO.class));
+    }
+
+    @Test
+    void testLogin() throws Exception {
+
+        when(userService.validateUser("João", "joao@email.com", "senha123")).thenReturn(true);
+
+        mockMvc.perform(post("/users/login")
+                            .contentType("application/json")
+                            .content("{\"name\":\"João\",\"email\":\"joao@email.com\",\"password\":\"senha123\"}"))
+                    .andExpect(status().isOk())
+                    .andExpect(content().string("Logado com sucesso"));
+
+        verify(userService, times(1)).validateUser("João", "joao@email.com", "senha123");
+    }
+}
