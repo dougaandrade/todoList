b368398b2cd2a24e610bc61f16273701518130b2
ViniDias1 <viniciusdvalenca@gmail.com>
2025-04-01 20:48:04 -0300

    feat: implement signup functionality with form handling and service integration

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { MainScreenComponent } from './main-screen/main-screen.component';
 import { BoardsComponent } from './boards/boards.component';
+import { CadastroComponent } from './signup/signUp.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'main', component: MainScreenComponent },
-  { path: 'boards', component: BoardsComponent }
+  { path: 'boards', component: BoardsComponent },
+  { path: 'signup', component: CadastroComponent }
 ];
